1e390fecab1443a79abb2c1e4900b5389ebb9fd1
achint <achkish@gmail.com>
2023-09-02 14:04:54 +0530

    first commit

---
+++ server/routes/movieRoute.js
@@ -0,0 +1,79 @@
+const router = require('express').Router()
+const authMiddleware = require('../middlewares/authMiddleware')
+const Movie = require('../models/movieModel')
+
+
+
+// Add a new Movie
+
+router.post('/add-movie' , async (req , res)=>{
+    try {
+        const newMovie = new Movie(req.body)
+        await newMovie.save()
+        res.send({
+            success: true,
+            message: "Movie added successfully",
+          });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+          });
+    }
+})
+
+router.get("/get-all-movies", async (req, res) => {
+    try {
+      const movies = await Movie.find()
+      res.send({
+        success: true,
+        message: "Movies fetched successfully",
+        data: movies,
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  })
+
+  // update a movie
+router.post("/update-movie", authMiddleware, async (req, res) => {
+    try {
+      await Movie.findByIdAndUpdate(req.body.movieId, req.body);
+      res.send({
+        success: true,
+        message: "Movie updated successfully",
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+
+
+  // // delete a movie
+router.post("/delete-movie", authMiddleware, async (req, res) => {
+    try {
+      await Movie.findByIdAndDelete(req.body.movieId);
+      res.send({
+        success: true,
+        message: "Movie deleted successfully",
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+
+
+
+
+module.exports = router;
+
+
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express= require('express')
+var cors = require('cors');
+
+
+const app = express()
+app.use(cors());
+require('dotenv').config()
+const dbConfig = require('./config/dbConfig')
+const userRoute = require('./routes/userRoute')
+const movieRoute = require('./routes/movieRoute')
+const theatreRoute = require('./routes/theatreRoute')
+
+app.use(express.json())
+app.use('/api/users' , userRoute)
+app.use('/api/movies' , movieRoute)
+app.use('/api/theatres' , theatreRoute)
+
+
+
+
+app.listen(8000 , ()=>{
+    console.log('Server is Running')
+})
